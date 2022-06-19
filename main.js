63425729f2c552e835062aa784ce9a70e77d7f44
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-06-19 21:48:26 +0200

    made upcoming events v1 + api testing (chippr)

--- main.js
+++ main.js
@@ -10,10 +10,30 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-  });
+// app.get("/", (req, res) => {
+//     res.render("index.ejs");
+//   });
+
+app.get('/', (req, res) => {
+    fetchJson("https://chipr.api.fdnd.nl/v1/project").then(function (jsonData) {
+        res.render('index', {
+          title: 'Dit is de chippr api',
+          projects: jsonData.data,
+        })
+      })
+    })
 
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+ async function fetchJson(url) {
+    return await fetch(url)
+      .then((response) => response.json())
+      .catch((error) => error)
+  }
