01e5672f2d00c305d241a1323dd863d29de12e90
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-06-29 16:12:40 +0200

    crud form add

--- main.js
+++ main.js
@@ -1,66 +1,87 @@
-const express = require('express')
-const { json } = require('express/lib/response')
-const app = express()
-const port = 3000
+const express = require("express");
+const { json } = require("express/lib/response");
+const app = express();
+const port = 3000;
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+var bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({extended:false})
 
-//serve public files 
-app.use(express.static('public'))
-
-// hook up template engine 
-app.set('view engine', 'ejs')
-app.set('views', './views',)
+app.use(express.urlencoded({extended: true}))  
 
 
 
+//serve public files
+app.use(express.static("public"));
 
-  
-  app.get("/", (req, res) => {
-    fetchJson("https://weloveweb.api.fdnd.nl/v1/session").then(function (jsonData) {
-    res.render('index', {
-      title: 'Dit is de we love web api',
-      sessions: jsonData.data
-    })
-  })
-})
-
-    app.get("/detail/:id", (req, res) => {
-      res.render('detail', {
-      })
-      })
-
-
-      app.get("/agenda", (req, res) => {
-        fetchJson("https://weloveweb.api.fdnd.nl/v1/session").then(function (jsonData) {
-        res.render('agenda', {
-          title: 'Dit is de we love web api',
-          sessions: jsonData.data
-        })
-      })
-    })
-
-
-      app.get('/pages/crud', (req, res) => {
-        fetchJson("https://weloveweb.api.fdnd.nl/v1/session").then(function (jsonData) {
-        res.render('./pages/crud', {
-          title: 'Dit is de we love web',
-          sessions: jsonData.data,
-        })
-      })
-    })
-
-    app.get("/pages/crud/edit", (req, res) => {
-      res.render("./pages/edit");
-    });
-
-    app.get("/pages/crud/add", (req, res) => {
-      res.render("./pages/add");
-    });
+// hook up template engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 
 
+app.get("/", (req, res) => {
+	fetchJson("https://weloveweb.api.fdnd.nl/v1/session").then(function (
+		jsonData
+	) {
+		res.render("index", {
+			title: "Dit is de we love web api",
+			sessions: jsonData.data,
+		});
+	});
+});
 
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
+app.get("/detail/:id", (req, res) => {
+	res.render("detail", {});
+});
+
+app.get("/agenda", (req, res) => {
+	fetchJson("https://weloveweb.api.fdnd.nl/v1/session").then(function (
+		jsonData
+	) {
+		res.render("agenda", {
+			title: "Dit is de we love web api",
+			sessions: jsonData.data,
+		});
+	});
+});
+
+app.get("/pages/crud", (req, res) => {
+	fetchJson("https://weloveweb.api.fdnd.nl/v1/session").then(function (
+		jsonData
+	) {
+		res.render("./pages/crud", {
+			title: "Dit is de we love web",
+			sessions: jsonData.data,
+		});
+	});
+});
+
+app.get("/pages/crud/edit", (req, res) => {
+	res.render("./pages/edit");
+});
+
+app.get("/pages/crud/add", (req, res) => {
+	res.render("./pages/add");
+});
+
+// Crud
+
+app.post("/pages/crud/add", urlencodedParser, async (req, res) => {
+	const postData = {
+		method: "POST",
+
+		body: JSON.stringify(req.body), 
+
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+  console.log(1, postData);
+
+	await fetchJson("https://weloveweb.api.fdnd.nl/v1/session", postData).then(function (data) {
+		res.send("succes");
+	});
+});
 
 /**
  * Wraps the fetch api and returns the response body parsed through json
@@ -68,7 +89,17 @@ app.listen(process.env.PORT || 3000, () => console.log(`App available on http://
  * @returns the json response from the api endpoint
  */
  async function fetchJson(url) {
-    return await fetch(url)
-      .then((response) => response.json())
-      .catch((error) => error)
-  }
+	return await fetch(url)
+		.then((response) => response.json())
+		.catch((error) => error);
+}
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () =>
+	console.log(`App available on http://localhost:3000`)
+);
+
